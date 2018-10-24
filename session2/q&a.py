3b6bbdba753ecae27a2bc2b6a5d5b4718de3a098
dattq1701@gmail.com <dattq1701@gmail.com>
2018-10-25 00:19:10 +0700

    update lan 3

---
+++ session2/q&a.py
@@ -0,0 +1,27 @@
+# 1. 
+# -   What is boolean ? 
+#     A Boolean value is either true or false
+
+# -   3 example
+#     +   1 == 2
+#     +   1 + 2 < 5
+#     +   "Alo" != "Omo"
+# 2.
+# -   What is a flow chart?
+#     Flow chart is a diagram of the sequence of movements or actions of people or things involved in a complex system or activity.
+
+# 3.
+# -   What is nested conditional?
+#     One conditional can also be nested within another.
+# - A piece of code uses nested conditionals
+
+n = int(input("Input your age: "))
+if n < 5:
+    print("Hello Baby")
+elif n < 16:
+    print("Hello bro")
+else:
+    print("Hello :D")
+
+
+    
